487f954b00dab814cdeb4d00168dcb026c97d4e5
Asjad Asif Jah <asjadjah@gmail.com>
2019-01-12 03:55:24 +0500

    Initial commit. Basic application created

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <People msg="People Data View"/>
+  </div>
+</template>
+
+<script>
+import People from './components/People.vue'
+
+export default {
+  name: 'app',
+  components: {
+    People
+  }
+}
+</script>
---
+++ src/components/People.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="People">
+        <h2>{{ msg }}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'People',
+        props: {
+            msg: String
+        }
+    }
+</script>
